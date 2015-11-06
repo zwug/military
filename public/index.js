f65e2bda1a1fa3dd7e9607cce573cae4db6eeb2c
Kirill Mironov <zwugme@gmail.com>
2015-11-06 13:16:12 +0300

    updated build

--- public/index.js
+++ public/index.js
@@ -45,7 +45,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(1);
-	module.exports = __webpack_require__(615);
+	module.exports = __webpack_require__(618);
 
 
 /***/ },
@@ -72,14 +72,9 @@
 
 	var render = _require.render;
 
-	var reducers = __webpack_require__(324);
-
-	var rootReducer = (0, _redux.combineReducers)({
-	  reducers: reducers
-	});
+	var rootReducer = __webpack_require__(324);
 
 	var finalCreateStore = (0, _redux.compose)((0, _reduxDevtools.devTools)())(_redux.createStore);
-
 	var store = finalCreateStore(rootReducer);
 	var routes = __webpack_require__(326);
 
@@ -90,11 +85,6 @@
 	    _reactRedux.Provider,
 	    { store: store },
 	    routes
-	  ),
-	  _react2.default.createElement(
-	    _react3.DebugPanel,
-	    { top: true, right: true, bottom: true },
-	    _react2.default.createElement(_react3.DevTools, { store: store, monitor: _react3.LogMonitor })
 	  )
 	), document.getElementById('root'));
 
@@ -26657,7 +26647,7 @@
 
 	var POPUP_TOGGLE = _require2.POPUP_TOGGLE;
 
-	function PopupToggle() {
+	function showPopup() {
 	  var state = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
 	  var action = arguments[1];
 
@@ -26669,11 +26659,11 @@
 	  }
 	}
 
-	var militaryApp = combineReducers({
-	  PopupToggle: PopupToggle
+	var rootReducer = combineReducers({
+	  showPopup: showPopup
 	});
 
-	module.exports = militaryApp;
+	module.exports = rootReducer;
 
 /***/ },
 /* 325 */
@@ -26684,10 +26674,10 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	exports.PopupToggle = PopupToggle;
+	exports.popupToggle = popupToggle;
 	var POPUP_TOGGLE = exports.POPUP_TOGGLE = 'POPUP_TOGGLE';
 
-	function PopupToggle() {
+	function popupToggle() {
 	  return {
 	    type: POPUP_TOGGLE
 	  };
@@ -26707,7 +26697,7 @@
 	var Route = _require.Route;
 
 	var App = __webpack_require__(373);
-	var Prl = __webpack_require__(612);
+	var Prl = __webpack_require__(615);
 
 	module.exports = React.createElement(
 	  Router,
@@ -31071,18 +31061,12 @@
 	'use strict';
 
 	var React = __webpack_require__(2);
-
-	var _require = __webpack_require__(168);
-
-	var connect = _require.connect;
-
-	var _require2 = __webpack_require__(325);
-
-	var PopupToggle = _require2.PopupToggle;
-
 	var Header = __webpack_require__(374);
 
 	var App = React.createClass({
+	  propTypes: {
+	    children: React.PropTypes.node
+	  },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
@@ -31093,13 +31077,7 @@
 	  }
 	});
 
-	function select(state) {
-	  return {
-	    showPopup: state.showPopup
-	  };
-	}
-
-	module.exports = connect(select)(App);
+	module.exports = App;
 
 /***/ },
 /* 374 */
@@ -31109,18 +31087,18 @@
 
 	var React = __webpack_require__(2);
 
-	var _require = __webpack_require__(327);
+	var _require = __webpack_require__(375);
 
-	var Link = _require.Link;
+	var Nav = _require.Nav;
+	var Navbar = _require.Navbar;
+	var NavBrand = _require.NavBrand;
+	var NavDropdown = _require.NavDropdown;
+	var NavItem = _require.NavItem;
+	var MenuItem = _require.MenuItem;
 
-	var _require2 = __webpack_require__(375);
+	var _require2 = __webpack_require__(612);
 
-	var Nav = _require2.Nav;
-	var Navbar = _require2.Navbar;
-	var NavBrand = _require2.NavBrand;
-	var NavDropdown = _require2.NavDropdown;
-	var NavItem = _require2.NavItem;
-	var MenuItem = _require2.MenuItem;
+	var LinkContainer = _require2.LinkContainer;
 
 	var Header = React.createClass({
 	  render: function render() {
@@ -31148,11 +31126,11 @@
 	          NavDropdown,
 	          { eventKey: 2, title: 'Dropdown', id: 'basic-nav-dropdown' },
 	          React.createElement(
-	            MenuItem,
-	            { eventKey: '1' },
+	            LinkContainer,
+	            { to: '/schemas/prl' },
 	            React.createElement(
-	              Link,
-	              { to: '/schemas/prl' },
+	              MenuItem,
+	              null,
 	              'ПРЛ'
 	            )
 	          ),
@@ -47321,24 +47299,234 @@
 
 	'use strict';
 
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	var _IndexLinkContainer2 = __webpack_require__(613);
+
+	var _IndexLinkContainer3 = _interopRequireDefault(_IndexLinkContainer2);
+
+	exports.IndexLinkContainer = _IndexLinkContainer3['default'];
+
+	var _LinkContainer2 = __webpack_require__(614);
+
+	var _LinkContainer3 = _interopRequireDefault(_LinkContainer2);
+
+	exports.LinkContainer = _LinkContainer3['default'];
+
+/***/ },
+/* 613 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _LinkContainer = __webpack_require__(614);
+
+	var _LinkContainer2 = _interopRequireDefault(_LinkContainer);
+
+	var IndexLinkContainer = (function (_React$Component) {
+	  _inherits(IndexLinkContainer, _React$Component);
+
+	  function IndexLinkContainer() {
+	    _classCallCheck(this, IndexLinkContainer);
+
+	    _get(Object.getPrototypeOf(IndexLinkContainer.prototype), 'constructor', this).apply(this, arguments);
+	  }
+
+	  _createClass(IndexLinkContainer, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2['default'].createElement(_LinkContainer2['default'], _extends({}, this.props, { onlyActiveOnIndex: true }));
+	    }
+	  }]);
+
+	  return IndexLinkContainer;
+	})(_react2['default'].Component);
+
+	exports['default'] = IndexLinkContainer;
+	module.exports = exports['default'];
+
+/***/ },
+/* 614 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// This is largely taken from react-router/lib/Link.
+
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactRouter = __webpack_require__(327);
+
+	var LinkContainer = (function (_React$Component) {
+	  _inherits(LinkContainer, _React$Component);
+
+	  function LinkContainer(props, context) {
+	    _classCallCheck(this, LinkContainer);
+
+	    _get(Object.getPrototypeOf(LinkContainer.prototype), 'constructor', this).call(this, props, context);
+
+	    this.onClick = this.onClick.bind(this);
+	  }
+
+	  _createClass(LinkContainer, [{
+	    key: 'onClick',
+	    value: function onClick(event) {
+	      if (this.props.disabled) {
+	        event.preventDefault();
+	        return;
+	      }
+
+	      if (this.props.children.props.onClick) {
+	        this.props.children.props.onClick(event);
+	      }
+
+	      _reactRouter.Link.prototype.handleClick.call(this, event);
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var history = this.context.history;
+	      var _props = this.props;
+	      var onlyActiveOnIndex = _props.onlyActiveOnIndex;
+	      var to = _props.to;
+	      var query = _props.query;
+	      var hash = _props.hash;
+	      var children = _props.children;
+
+	      var props = _objectWithoutProperties(_props, ['onlyActiveOnIndex', 'to', 'query', 'hash', 'children']);
+
+	      delete props.state;
+
+	      props.onClick = this.onClick;
+
+	      // Ignore if rendered outside the context of history, simplifies unit testing.
+	      if (history) {
+	        props.href = history.createHref(to, query);
+
+	        if (hash) {
+	          props.href += hash;
+	        }
+
+	        props.active = history.isActive(to, query, onlyActiveOnIndex);
+	      }
+
+	      return _react2['default'].cloneElement(_react2['default'].Children.only(children), props);
+	    }
+	  }]);
+
+	  return LinkContainer;
+	})(_react2['default'].Component);
+
+	exports['default'] = LinkContainer;
+
+	LinkContainer.propTypes = {
+	  onlyActiveOnIndex: _react2['default'].PropTypes.bool.isRequired,
+	  to: _react2['default'].PropTypes.string.isRequired,
+	  query: _react2['default'].PropTypes.object,
+	  hash: _react2['default'].PropTypes.string,
+	  state: _react2['default'].PropTypes.object,
+	  onClick: _react2['default'].PropTypes.func,
+	  disabled: _react2['default'].PropTypes.bool.isRequired,
+	  children: _react2['default'].PropTypes.node.isRequired
+	};
+
+	LinkContainer.contextTypes = {
+	  history: _react2['default'].PropTypes.object
+	};
+
+	LinkContainer.defaultProps = {
+	  onlyActiveOnIndex: false,
+	  disabled: false
+	};
+	module.exports = exports['default'];
+
+/***/ },
+/* 615 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
 	var React = __webpack_require__(2);
-	var ModalInfo = __webpack_require__(613);
+
+	var _require = __webpack_require__(168);
+
+	var connect = _require.connect;
+
+	var ModalInfo = __webpack_require__(616);
+
+	var _require2 = __webpack_require__(325);
+
+	var popupToggle = _require2.popupToggle;
 
 	var Prl = React.createClass({
+	  propTypes: {
+	    popupToggle: React.PropTypes.func.isRequired,
+	    showPopup: React.PropTypes.bool.isRequired
+	  },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
 	      { className: 'container' },
-	      React.createElement('div', { className: 'schema-block br009', 'data-toggle': 'modal', 'data-target': '#myModalInfo' }),
-	      React.createElement(ModalInfo, { show: true })
+	      React.createElement('div', { className: 'schema-block br009', onClick: this.props.popupToggle }),
+	      React.createElement(ModalInfo, { show: this.props.showPopup, onHide: this.props.popupToggle })
 	    );
 	  }
 	});
 
-	module.exports = Prl;
+	function mapStateToProps(state) {
+	  return {
+	    showPopup: state.showPopup
+	  };
+	}
+
+	module.exports = connect(mapStateToProps, {
+	  popupToggle: popupToggle
+	})(Prl);
 
 /***/ },
-/* 613 */
+/* 616 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -47372,7 +47560,7 @@
 	        React.createElement(
 	          Modal.Body,
 	          null,
-	          React.createElement('img', { className: 'modal-info-image', src: __webpack_require__(614) }),
+	          React.createElement('img', { className: 'modal-info-image', src: __webpack_require__(617) }),
 	          React.createElement(
 	            'p',
 	            null,
@@ -47472,13 +47660,13 @@
 	module.exports = ModalInfo;
 
 /***/ },
-/* 614 */
+/* 617 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = __webpack_require__.p + "images/br009.png"
 
 /***/ },
-/* 615 */
+/* 618 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
